88d666ae2bf18c0759b6050686ac4b6c8b077b08
杨伟韦 <865562227@qq.com>
2020-08-27 17:06:58 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <hello-world></hello-world>
+    </div>
+</template>
+
+<script lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+export default {
+    components: {
+        HelloWorld
+    },
+    data() {
+        return {
+            msg: 'hello world'
+        }
+    },
+}
+</script>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <div style="text-align: center; font-size: 32px;">{{msg}}</div>
+        <div  style="text-align: center">{{moreMsg}}</div>
+    </div>
+</template>
+
+<script lang="ts">
+class Msg {
+    public msg: string = 'hello world';
+    GetMoreMsg(): string{
+        return'webpack+vue+typescript+less'
+    }
+}
+//创建msg对象
+let MsgObj = new Msg();
+export default {
+    name: 'Hello',
+    data() {
+        return {
+            msg: MsgObj.msg,
+            moreMsg: MsgObj.GetMoreMsg()
+        }
+    },
+}
+</script>
+
+
+
